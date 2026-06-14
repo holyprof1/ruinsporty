5d144b6f9b5aaaf6ad954a100c372662823f0d17
holyprof1 <holyprofweb@gmail.com>
2026-06-14 10:30:00 +0100

    feat: add express server skeleton with static file serving

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.listen(PORT, () => console.log("Server running on port " + PORT));
