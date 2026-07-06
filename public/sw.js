1a3f82257922b7607962f09c354ce91b7074a4a3
holyprof1 <holyprofweb@gmail.com>
2026-07-06 12:56:39 +0100

    fix: bump asset versions to bust browser + service worker cache

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,4 @@
+// v2
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
